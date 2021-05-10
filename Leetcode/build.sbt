27c1d17b9d692ba6027acc53611e2e18ad8f5de1
rbrtknwls <r2knowle@uwaterloo.ca>
2021-05-10 15:39:18 -0700

    First Scala Solution:

---
+++ Leetcode/build.sbt
@@ -0,0 +1,5 @@
+name := "Leetcode"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
---
+++ Leetcode/src/main/scala/Main.scala
@@ -0,0 +1,50 @@
+import scala.collection.mutable.ArrayBuffer
+
+object Solution_20 {
+  var accum: String = "";
+  def isValid(s: String): Boolean = {
+    s match {
+      case "" =>
+        val x = accum == ""
+        accum = ""
+        x
+      case s if List("(", "[", "{") contains s.substring(0,1) =>
+        accum = s.substring(0,1) + accum
+        isValid(s.substring(1))
+
+      case s if accum == "" => false
+      case s if s.substring(0,1) == ")" =>
+        if (accum.substring(0,1) == "(") {
+          accum = accum.substring(1)
+          isValid(s.substring(1))
+        } else {
+          accum = ""
+          false
+        }
+
+      case s if s.substring(0,1) == "]" =>
+        if (accum.substring(0,1) == "["){
+          accum = accum.substring(1)
+          isValid(s.substring(1))
+        } else {
+          accum = ""
+          false
+
+        }
+      case s if s.substring(0,1) == "}" => {
+        if (accum.substring(0,1) == "{"){
+          accum = accum.substring(1)
+          isValid(s.substring(1))
+        } else {
+          accum = ""
+          false
+
+        }
+      }
+    }
+  }
+}
+
+object Main extends App {
+
+}
